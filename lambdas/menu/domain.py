12eb6641ff30fd1eef3d118ee90a4b06ff11cc6f
pool <loop.edward@gmail.com>
2022-06-29 23:01:15 +0800

    Update menu and menu_item domain

--- lambdas/menu/domain.py
+++ lambdas/menu/domain.py
@@ -1,29 +1,23 @@
+from decimal import Decimal
+from datetime import datetime
+
+
+class MenuItem:
+
+    def __init__(self, **kwargs):
+        self.description = kwargs["description"]
+        self.price = Decimal(kwargs["price"])
+        self.category = kwargs["category"]
+
 class Menu:
 
     def __init__(self, **kwargs):
         self.merchant_id = kwargs["merchantId"]
         self.name = kwargs["name"]
-        self.products = []
+        self.items = []
         self.categories = []
+        self.status = "ACTIVE"
+        self.date_created = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-    def add_category(self, name):
-        category = Category(name)
-        self.categories.append(category)
-        return category
-
-    def add_product(self, product):
-        self.products.append(product)
-
-
-class Category:
-
-    def __init__(self, name):
-        self.name = name
-
-
-class Product:
-
-    def __init__(self, **kwargs):
-        self.description = kwargs["description"]
-        self.price = kwargs["price"]
-        self.category = kwargs["category"]
+    def add_menu_item(self, menu_item: MenuItem):
+        self.items.append(menu_item)
