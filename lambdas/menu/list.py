2ce0e68311b5e346a3ae67573ee4dabbc4bf91cf
pool <loop.edward@gmail.com>
2022-07-01 23:32:24 +0800

    Initial commit for list menu

---
+++ lambdas/menu/list.py
@@ -0,0 +1,13 @@
+from request import http_response_ok
+from repository import MenuRepository
+
+repository = MenuRepository()
+
+
+@http_response_ok
+def handler(event, context):
+    merchant_id = event["pathParameters"]["merchantId"]
+    print(f"Merchant ID: {merchant_id}")
+    menus = repository.find_all_by_merchant_id(merchant_id)
+    print(menus)
+    return {"a": "b"}
--- lambdas/menu/repository.py
+++ lambdas/menu/repository.py
@@ -8,6 +8,18 @@ class MenuRepository:
         self._dynamodb = boto3.resource("dynamodb")
         self._table = self._dynamodb.Table("bohr-menu")
 
+    def find_all_by_merchant_id(self, merchant_id):
+        results = self._table.query(
+            KeyConditionExpression="#pk = :pk",
+            ExpressionAttributeNames={
+                "#pk": "pk"
+            },
+            ExpressionAttributeValues={
+                ":pk": f"MERCHANT#{merchant_id}"
+            }
+        )
+        return results
+
     def save(self, menu: Menu):
         db_item = serialize_menu(menu)
         db_item["pk"] = f"MERCHANT#{menu.merchant_id}"
