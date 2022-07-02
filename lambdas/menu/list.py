9ab524828812e72c5e5dd5db57c2959982787912
pool <loop.edward@gmail.com>
2022-07-03 07:34:47 +0800

    Remove print statements

--- lambdas/menu/list.py
+++ lambdas/menu/list.py
@@ -8,7 +8,5 @@ repository = MenuRepository()
 @http_response_ok
 def handler(event, context):
     merchant_id = event["pathParameters"]["merchantId"]
-    print(f"Merchant ID: {merchant_id}")
     menus = repository.find_all_by_merchant_id(merchant_id)
-    print(menus)
     return [marshal_menu_metadata(menu) for menu in menus]
