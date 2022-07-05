29d169e130994e66213fc4a9624ed6249ab47612
pool <loop.edward@gmail.com>
2022-07-05 18:03:18 +0800

    Extract logic to service

--- lambdas/menu/api.py
+++ lambdas/menu/api.py
@@ -1,43 +1,40 @@
 import json
-from re import I
+from domain import Menu
+from service import MenuManagementService
 from marshaller import marshal_menu, marshal_menu_metadata
 from repository import MenuRepository
 from request import MenuMapper, http_response_ok
-from uuid import uuid4
-from datetime import datetime
 
 
 request_mapper = MenuMapper()
 repository = MenuRepository()
+service = MenuManagementService()
 
 
 @http_response_ok
-def create(event, context):
+def create(event, _):
     request_body = dict(json.loads(event['body']))
     request_body["merchant_id"] = event["pathParameters"]["merchantId"]
 
-    menu = request_mapper.map(request_body)
-    menu.id = str(uuid4())[-6:]
-    menu.status = "INACTIVE"
-    menu.date_created = datetime.now()
-    repository.save(menu)
+    menu: Menu = request_mapper.map(request_body)
+    menu = service.create(menu)
 
     return marshal_menu(menu)
 
 
 @http_response_ok
-def list(event, context):
+def list(event, _):
     merchant_id = event["pathParameters"]["merchantId"]
-    menus = repository.find_all_by_merchant_id(merchant_id)
+    menus: list = service.find_all_metadata_by_merchant_id(merchant_id)
 
     return [marshal_menu_metadata(menu) for menu in menus]
 
 
 @http_response_ok
-def get(event, context):
+def get(event, _):
     merchant_id = event["pathParameters"]["merchantId"]
     menu_id = event["pathParameters"]["menuId"]
-    menu = repository.find_by_merchant_and_menu(merchant_id, menu_id)
+    menu = service.find_by_merchant_and_menu(merchant_id, menu_id)
 
     if(menu is None):
         return None
@@ -46,21 +43,11 @@ def get(event, context):
 
 
 @http_response_ok
-def activate(event, context):
+def activate(event, _):
     merchant_id = event["pathParameters"]["merchantId"]
     menu_id = event["pathParameters"]["menuId"]
 
-    # check if menu is there
-    menus = repository.find_all_by_merchant_id(merchant_id)
-    for menu in menus:
-        if menu.id == menu_id:
-            menu.activate()
-        else:
-            menu.deactivate()
+    menu = repository.find_by_merchant_and_menu(merchant_id, menu_id)
+    service.activate(menu)
 
-    repository.batch_save_metadata(menus)
-
-    return {
-        "merchant_id": merchant_id,
-        "menu_id": menu_id
-    }
+    return marshal_menu(menu)
